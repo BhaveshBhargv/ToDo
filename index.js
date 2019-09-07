1ffc985cd28c921ff00b455e8db9797a6ac81ba0
bhaveshbhargv <@bhavesh.bhargv@gamil.com>
2019-09-07 17:13:14 +0530

    first commmit

---
+++ index.js
@@ -0,0 +1,14 @@
+function add(){
+
+let input = document.getElementById('input').value;
+let a = document.createTextNode(input);
+let li = document.createElement('li');
+li.appendChild(a);
+let task = document.getElementById('todo');
+if (input === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("todo").appendChild(li);
+  }
+  document.getElementById('input').value = "";
+}
